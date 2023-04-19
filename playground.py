2a419c7374956ed67b03d810feaebe6d596c1e15
prateek <prateek.patel@bricklane.com>
2023-04-19 12:54:06 +0100

    specifying optimizer separately to inspect internals

--- playground.py
+++ playground.py
@@ -18,23 +18,25 @@ outputs = pk.normalize_minmax(outputs, array_min, array_max)
 # Create model
 fish_classifier = pk.LinearRegression(inputs_poly.shape[1], 1)
 
+opt = pk.Momentum(learning_rate=0.2, decay_rate=0.9)
+
 # Train the model
 fish_classifier.train(
     training_data=inputs_poly,
     targets=outputs,
     batch_size=22,
-    epochs=500,
-    optimizer=pk.GradientDescent(learning_rate=0.01, decay_rate=0.9),
+    epochs=200,
+    optimizer=opt,
     testing_freq=1,
     decay_freq=10
 )
-
-# Save model
-pk.save(fish_classifier, 'fish_classifier.pkl')
 
 # Plot performance
 fish_classifier.plot_performance()
 
 # Print r2 score
 print('r2score:', fish_classifier.r2score(inputs_poly, outputs))
+
+# Save model
+pk.save(fish_classifier, 'fish_classifier.pkl')
 
