d803f4885c7df6cf5994bf3d0a3d5f9c8dad3dd6
prateek <prateek.patel@bricklane.com>
2023-04-24 21:10:13 +0100

    adding adapter to play around easily like scikit learn

--- playground.py
+++ playground.py
@@ -1,5 +1,31 @@
 import pykitml as pk
 from pykitml.datasets import fishlength
+import matplotlib.pyplot as plt
+
+
+class PKtoSKAdapter:
+    def __init__(self, model, **train_kwargs):
+        self._model = model
+        self.train_kwargs = train_kwargs
+
+    def fit(self, X, y):
+        self._model.train(X, y, **self.train_kwargs)
+
+    def predict(self, X):
+        self._model.feed(X)
+        return self._model.get_output()
+
+    def set_params(self, **kwargs):
+        self.train_kwargs.update(**kwargs)
+        return self.train_kwargs
+
+    def __getattr__(self, attr):
+        """All non-adapted calls are passed to the object"""
+        if attr == "_model":  # Prevent infinite recursion on model load
+            pass
+        else:
+            return getattr(self._model, attr)
+
 
 # Load the dataset
 inputs, outputs = fishlength.load()
@@ -18,25 +44,29 @@ outputs = pk.normalize_minmax(outputs, array_min, array_max)
 # Create model
 fish_classifier = pk.LinearRegression(inputs_poly.shape[1], 1)
 
-opt = pk.Momentum(learning_rate=0.2, decay_rate=0.9)
+opt = pk.Momentum(learning_rate=0.2)
+
+model = PKtoSKAdapter(fish_classifier,
+                      batch_size=22,
+                      epochs=2000,
+                      optimizer=opt,
+                      testing_freq=1,
+                      decay_freq=10)
 
 # Train the model
-fish_classifier.train(
-    training_data=inputs_poly,
-    targets=outputs,
-    batch_size=22,
-    epochs=200,
-    optimizer=opt,
-    testing_freq=1,
-    decay_freq=10
-)
+model.fit(inputs_poly, outputs)
 
 # Plot performance
-fish_classifier.plot_performance()
+model.plot_performance()
 
 # Print r2 score
-print('r2score:', fish_classifier.r2score(inputs_poly, outputs))
+print('r2score:', model.r2score(inputs_poly, outputs))
+plt.scatter(outputs, model.predict(inputs_poly))
+plt.show()
 
 # Save model
-pk.save(fish_classifier, 'fish_classifier.pkl')
+pk.save(model, 'fish_classifier.pkl')
 
+mod = pk.load('fish_classifier.pkl')
+
+mod.predict(inputs_poly)
